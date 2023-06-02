b1a7428092dc80635d6938815c525920e5d2687e
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-06-02 16:18:22 +0200

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,10 @@
+import os
+import json
+import sys
+
+with open("temp/config.json") as f:
+    d = json.load(f, strict=False)
+os.system("echo '"+d["country"] + "\n' > temp/country")
+os.system("echo 'https://"+sys.argv[1]+"@"+d["repo"] + "' > temp/repo")
+for key in d["keys"]:          
+    os.system("echo '"+key + "' >> temp/gpg")
