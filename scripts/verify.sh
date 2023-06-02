7579b9bcc7d4ff5582bf96a883a30405eeeadef4
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-06-02 16:52:38 +0200

    Create verify.sh

---
+++ scripts/verify.sh
@@ -0,0 +1,5 @@
+gpg --import temp/gpg 2> /dev/null
+cd repo
+(cat ../temp/tag | xargs git verify-tag --raw 2> >(grep "GOODSIG"))>../temp/verifyResult
+cd ..
+gpg --list-keys --with-colons | grep fpr| awk 'NR % 2== 0' | awk -F ':' '{print $10}' | xargs gpg --batch --yes --delete-keys
