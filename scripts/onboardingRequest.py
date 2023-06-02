c6757c79c4284207495a1e30b9f879b1fb3c18c9
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-06-02 16:19:32 +0200

    Create onboardingRequest.py

---
+++ scripts/onboardingRequest.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+
+from datetime import datetime
+
+date_time = datetime.now()
+
+with open('temp/verifyResult') as f:
+  result = f.readline()
+  
+  if not result:
+      raise Exception("Bad verification.")
+  else:
+      print("Verification was good.")
+
+with open('temp/country') as f:
+  country = f.readline().replace("\n","")
+  
+branchName = country+"/onboardingRequest" 
+
+os.system("git checkout -b" + branchName)
+os.system("rm -rf "+ country)
+os.system("mkdir " + country)
+os.system("mv  -v repo/onboarding " + country + "/" )
+os.system("rm country" +"/Readme.md")
+os.system("git add "+ country)
+os.system("git commit -m 'Bot added Files from "+country+"'")
+os.system("git push -f -u origin "+ branchName)
