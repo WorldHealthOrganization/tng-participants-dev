80135c3d787aa8090f325a49241e7fb16db93879
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-06-06 16:29:47 +0200

    Update delivery.sh

--- scripts/delivery.sh
+++ scripts/delivery.sh
@@ -1,7 +1,7 @@
 mkdir temp
 echo $1 > temp/config.json
 sed -i "s/'/\"/g" temp/config.json
-python scripts/config.py $2 1> /dev/null
-python scripts/repo.py 1> /dev/null
+python scripts/config.py $2
+python scripts/repo.py 
 ./scripts/verify.sh 1> /dev/null
 python scripts/onboardingRequest.py
