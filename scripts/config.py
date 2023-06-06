8155835d06a39a86b24eb6a47930313205102bbf
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-06-06 17:06:18 +0200

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,9 +1,12 @@
 import os
 import json
 import sys
+import base64
 
-with open("temp/config.json") as f:
-    d = json.load(f, strict=False)
+with open("temp/base64") as f:
+    decodedBytes = base64.urlsafe_b64decode(f.read())
+    decodedStr = str(decodedBytes,"utf-8")
+d = json.loads(decodedStr, strict=False)
 os.system("echo '"+d["country"] + "\n' > temp/country")
 os.system("echo 'https://"+sys.argv[1]+"@"+d["repo"] + "' > temp/repo")
 for key in d["keys"]:          
