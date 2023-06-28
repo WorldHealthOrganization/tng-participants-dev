b9e7a5e6edf6db53d6fd7838ec7dcd4edb6c7c6c
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-28 21:02:54 +0200

    script fix

--- scripts/transitiveTrust.sh
+++ scripts/transitiveTrust.sh
@@ -1,10 +1,10 @@
-git clone https://${BOT_TOKEN_GITHUB}@${TRANSITIVE_TRUST_SOURCE} transit
+git clone https://$BOT_TOKEN_GITHUB@$TRANSITIVE_TRUST_SOURCE transit
 cd transit
 cd $ENV
 mkdir signing 
 cd signing     
-echo "${secrets.NB_UP_SIGNING_PUB}" > pub-key.pem
-echo "${secrets.NB_UP_SIGNING_KEY}" > priv-key.pem
+echo "$NB_UP_SIGNING_PUB}" > pub-key.pem
+echo "$NB_UP_SIGNING_KEY" > priv-key.pem
 cd .. 
 cd ..
 ./extract.sh $ENV $1     
