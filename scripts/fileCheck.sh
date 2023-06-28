e62a9f91eedaf2993df382ead08b170a4492a5fc
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-28 22:39:22 +0200

    fix script

--- scripts/fileCheck.sh
+++ scripts/fileCheck.sh
@@ -3,25 +3,20 @@ echo "Start fileCheck"
 echo "Check:" $1
 find $1 -print | grep -i "Failure" > temp/Failure
 
-if [ -s "temp/Failure" ]; then
+if [ -n "temp/Failure" ]; then
     rm "temp/Failure"
 fi
 
 find $1 -print | grep -i "CSR" > temp/CSR
 
-if [ -s "temp/CSR" ]; then
+if [ -n "temp/CSR" ]; then
     rm "temp/CSR"
 fi
 
 find $1 -print | grep -i "signed" > temp/SIGNED
 
-if [ -s "temp/SIGNED" ]; then
+if [ -n "temp/SIGNED" ]; then
     rm "temp/SIGNED"
 fi
-echo failure
-cat temp/Failure
-echo csr
-cat temp/CSR
-echo signed
-cat temp/SIGNED
+
 echo "finished fileCheck"
