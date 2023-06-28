23aefb24f8b4cff0cf594eea573071628ca1334d
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-28 23:27:51 +0200

    token modification

--- scripts/delivery.py
+++ scripts/delivery.py
@@ -69,7 +69,8 @@ if __name__=='__main__':
                           raise Exception("Onboarding Request failed.")
                     except Exception as Error:
                         os.system("echo 'Error occoured for onboarding request " + country.alpha_3 +": "+str(Error)+"'") 
-                    
+                
+                os.environ["GH_TOKEN"] = os.environ.get("GITHUB_TOKEN")  
                 os.system("gh pr create -B main -H " + country.alpha_3 +"/onboardingRequest --title 'Bot requested a change for "+country.alpha_3+".' --body 'Please merge the onnboarding request of "+country.alpha_3+".' > /dev/null 2>&1" )
                 
                 if os.path.exists(country.alpha_3+"/onboarding/UP"):
@@ -81,6 +82,8 @@ if __name__=='__main__':
                 if os.path.exists(country.alpha_3+"/onboarding/ISSUER"):
                  os.system("./scripts/fileCheck.sh "+country.alpha_3+"/onboarding/ISSUER")
                 
+                os.environ["GH_TOKEN"] = os.environ.get("BOT_TOKEN_GITHUB")  
+               
                 if os.path.exists("temp/Failure"):
                     os.system("gh pr review "+country.alpha_3 +"/onboardingRequest -r -b 'Please resolve the Errors before proceeding. The failure files contain more information.'")
                 
@@ -89,7 +92,7 @@ if __name__=='__main__':
                 
                 if os.path.exists("temp/SIGNED"):
                    os.system("gh pr review "+country.alpha_3 +"/onboardingRequest -r -b 'The content is currently not signed. Run the sign script before merging'")
-                   
+                os.environ.pop("GH_TOKEN")
                 os.system("git checkout main > /dev/null 2>&1")
                 os.system("git reset --hard && git clean -f -d > /dev/null 2>&1")
             except Exception as Error:
