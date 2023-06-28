4677c5734efcdcadfcd820fb5740f856e06e0f8c
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 00:40:46 +0200

    env modification

--- scripts/createPR.sh
+++ scripts/createPR.sh
@@ -1,7 +1,4 @@
-echo $BOT_TOKEN_GITHUB > token
-gh auth login --with-token < token
-rm token
+export GITHUB_TOKEN = $GITHUB_JOB_TOKEN
 
 gh pr create -B main -H $1/onboardingRequest --title 'Bot requested a change for $1' --body 'Please merge the onnboarding request of $1.' > /dev/null 2>&1
                 
-gh auth logout -h github.com
--- scripts/review.sh
+++ scripts/review.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo $GITHUB_JOB_TOKEN > token
-gh auth login --with-token --host github.com < token 
-rm token
+export GITHUB_TOKEN = $BOT_TOKEN_GITHUB
 
 if [ -e "temp/Failure" ]; then
   gh pr review $1/onboardingRequest -r -b "Please resolve the Errors before proceeding. The failure files contain more information." 
@@ -15,5 +13,3 @@ fi
 if [ -e "temp/SIGNED" ]; then
   gh pr review $1/onboardingRequest -r -b "The content is currently not signed. Run the sign script before merging"
 fi
-
-gh auth logout -h github.com
