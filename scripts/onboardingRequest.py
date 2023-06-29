8e572d0ac03042ef2820def477d9536b3075a949
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 10:30:40 +0200

    script fix

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -17,13 +17,9 @@ with open('temp/country') as f:
 branchName = country+"/onboardingRequest" 
 os.system("git checkout -b" + branchName +" > /dev/null 2>&1")
 os.system("mkdir -p " + country)
+os.system("rm -rf " + country)
+
 os.system("mkdir -p " + country+"/onboarding")
-
-os.system("rm -f "+ country+"/onboarding/DCC/TLS/*.PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/SCA/*.PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/UP/UP_*[0-9].PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/ISSUER/*.txt")
-
 os.system("cp -r "+repo+"/onboarding " + country )
 
 os.system("[ -d "+country + "/onboarding/DCC/auth"+" ] && mv " + country + "/onboarding/DCC/auth "+ country+"/onboarding/DCC/TLS")
