8a79c96f872a0b87a0cffa465053df8acf8ebcd3
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 10:17:10 +0200

    test

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -27,22 +27,11 @@ os.system("mkdir -p " + country+"/onboarding")
 #os.system("rm -f "+ country+"/onboarding/DCC/UP/UP_*[0-9].PEM")
 #os.system("rm -f "+ country+"/onboarding/DCC/ISSUER/*.txt")
 
-
-
-os.system("echo ls before")
-os.system("ls "+country+"/onboarding" )
-os.system("echo  copy folder ")
-
 os.system("cp -r "+repo+"/onboarding " + country )
 
 os.system("echo ls ")
-os.system("ls "+repo+"/onboarding" )
+os.system("ls "+country+"/onboarding/DCC")
 
-os.system("echo ls ")
-os.system("ls "+country+"/onboarding" )
-
-os.system("echo ls ")
-os.system("ls "+country+"/onboarding/onboarding" )
 
 os.system("[ -d "+country + "/onboarding/DCC/auth"+" ] && mv " + country + "/onboarding/DCC/auth "+ country+"/onboarding/DCC/TLS")
 os.system("[ -d "+country + "/onboarding/DCC/csca"+" ] && mv " + country + "/onboarding/DCC/csca "+ country+"/onboarding/DCC/SCA")
