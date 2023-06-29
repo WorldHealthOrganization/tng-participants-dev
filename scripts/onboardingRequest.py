b1062cf2017731d7d9bf2cee4b26448b41c60e15
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 19:13:57 +0200

    git update

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -15,8 +15,14 @@ with open('temp/country') as f:
   country = f.readline().replace("\n","")
 
 branchName = country + "/onboardingRequest"
-os.system("git checkout -b" + branchName +" origin/"+branchName +"> /dev/null 2>&1")
-os.system("ls")
+
+branches = os.popen('git branch -').read()
+      
+if branchName in branches:
+  os.system("git checkout origin/"+branchName +"") 
+else:
+  os.system("git checkout -b" + branchName)
+  
 os.system("mkdir -p " + country)
 os.system("mkdir -p " + country+"/onboarding")
 os.system("cp -r "+repo+"/onboarding " + country )
@@ -26,6 +32,10 @@ os.system("[ -d "+country + "/onboarding/DCC/up"+" ] && mv " + country + "/onboa
 os.system("[ -f "+country + "/onboarding/DCC/SCA/CSCA.pem"+" ] && mv " + country + "/onboarding/DCC/SCA/CSCA.pem "+ country+"/onboarding/DCC/SCA/SCA.pem")
 os.system("git add "+ country)
 os.system("git commit -m 'Bot added Files from "+country+"' ")
-os.system("git push -f -u origin "+ branchName + " ")
+
+if branchName in branches:  
+  os.system("git push -f -u origin "+ branchName + " ")
+else:
+  os.system("git push origin "+ branchName + " ") 
 
 #> /dev/null 2>&1
