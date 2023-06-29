b394502721ce926be5aee3257e81b26ddd8f77c6
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 23:29:57 +0200

    review bot script modified

---
+++ scripts/tests/_test_transitive_trust.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+def test_transitive_trust_failure(country_folder,country):
+    approve = True
+    noFailure = True
+    signedFolderPresent = True
+    csrNotSigned = True
+    csrNotPresent = True
+    pathlist = Path(country_folder+"/onboarding/UP").glob('**/Failure')
+    
+    if len(pathlist): 
+        approve &= False
+        noFailure &= False
+        
+    if not (os.path.exists(country_folder+"/onboarding/UP/signed") and os.path.exists(country_folder+"/onboarding/TLS/signed") and os.path.exists(country_folder+"/onboarding/SCA/signed") and os.path.exists(country_folder+"/onboarding/ISSUER/signed")):  
+        signedFolderPresent &= False
+        approve &= False
+      
+    if  os.path.exists(country_folder+"/onboarding/UP/UP_SYNC.CSR"):  
+        csrNotSigned &= False
+        approve &= False
+        
+    if  os.path.exists(country_folder+"/onboarding/UP/UP_SYNC.PEM") and os.path.exists(country_folder+"/onboarding/UP/UP_SYNC.CSR"):  
+        csrNotPresent &= False
+        approve &= False
+        
+    if not noFailure:
+        os.system("gh pr review "+country+"/onboardingRequest -r -b 'Folder contains Failure files. Please resolve it.'")
+       
+    if not signedFolderPresent:
+        os.system("gh pr review "+country+"/onboardingRequest -r -b 'Signed Folder not present.'")
+       
+    if not csrNotSigned: 
+        os.system("gh pr review "+country+"/onboardingRequest -r -b 'CSR is not signed for UP. Please sign it.'")
+        
+    if not csrNotPresent: 
+        os.system("gh pr review "+country+"/onboardingRequest -r -b 'CSR is still present, but already signed.'")
+          
+    if approve:
+        os.system("gh pr review "+country+"/onboardingRequest -a -b 'Everything fine.'")
