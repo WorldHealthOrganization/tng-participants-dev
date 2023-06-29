08d8ddf2d626ff3b188ac3e943fce128db7c1318
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 18:15:34 +0200

    test

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -16,16 +16,16 @@ with open('temp/country') as f:
 
 branchName = country + "/onboardingRequest"
 os.system("git checkout -b" + branchName +" > /dev/null 2>&1")
-os.system("mkdir -p " + country)
-os.system("rm -rf " + country)
 
+os.system("mkdir -p " + country)
 os.system("mkdir -p " + country+"/onboarding")
 os.system("cp -r "+repo+"/onboarding " + country )
-
 os.system("[ -d "+country + "/onboarding/DCC/auth"+" ] && mv " + country + "/onboarding/DCC/auth "+ country+"/onboarding/DCC/TLS")
 os.system("[ -d "+country + "/onboarding/DCC/csca"+" ] && mv " + country + "/onboarding/DCC/csca "+ country+"/onboarding/DCC/SCA")
 os.system("[ -d "+country + "/onboarding/DCC/up"+" ] && mv " + country + "/onboarding/DCC/up "+ country+"/onboarding/DCC/UP")
 os.system("[ -f "+country + "/onboarding/DCC/SCA/CSCA.pem"+" ] && mv " + country + "/onboarding/DCC/SCA/CSCA.pem "+ country+"/onboarding/DCC/SCA/SCA.pem")
 os.system("git add "+ country)
-os.system("git commit -m 'Bot added Files from "+country+"' > /dev/null 2>&1")
-os.system("git push -f -u origin "+ branchName + " > /dev/null 2>&1")
+os.system("git commit -m 'Bot added Files from "+country+"' ")
+os.system("git push -u origin "+ branchName + " ")
+
+# > /dev/null 2>&1
