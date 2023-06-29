1289649950dd872a3d2627f418760ad25877e77a
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 10:22:25 +0200

    log enabled

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -33,10 +33,13 @@ os.system("echo ls ")
 os.system("ls "+country+"/onboarding/DCC")
 
 
+
 os.system("[ -d "+country + "/onboarding/DCC/auth"+" ] && mv " + country + "/onboarding/DCC/auth "+ country+"/onboarding/DCC/TLS")
 os.system("[ -d "+country + "/onboarding/DCC/csca"+" ] && mv " + country + "/onboarding/DCC/csca "+ country+"/onboarding/DCC/SCA")
 os.system("[ -d "+country + "/onboarding/DCC/up"+" ] && mv " + country + "/onboarding/DCC/up "+ country+"/onboarding/DCC/UP")
 os.system("[ -f "+country + "/onboarding/DCC/SCA/CSCA.pem"+" ] && mv " + country + "/onboarding/DCC/SCA/CSCA.pem "+ country+"/onboarding/DCC/SCA/SCA.pem")
 os.system("git add "+ country)
-os.system("git commit -m 'Bot added Files from "+country+"' > /dev/null 2>&1")
-os.system("git push -f -u origin "+ branchName + " > /dev/null 2>&1")
+os.system("git commit -m 'Bot added Files from "+country+"'")
+os.system("git push -f -u origin "+ branchName + "")
+
+#> /dev/null 2>&1
