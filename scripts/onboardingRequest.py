41fd7243a618bbc516238ebf21ce3a04e1493583
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-06-29 09:34:04 +0200

    test

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -18,11 +18,12 @@ branchName = country+"/onboardingRequest"
 os.system("git checkout -b" + branchName +" > /dev/null 2>&1")
 os.system("mkdir -p " + country)
 
-os.system("rm -f "+ country+"/onboarding/DCC/TLS/*.PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/SCA/*.PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/UP/UP_*[0-9].PEM")
-os.system("rm -f "+ country+"/onboarding/DCC/ISSUER/*.txt")
-
+os.system("rm -rf " + country)
+#os.system("rm -f "+ country+"/onboarding/DCC/TLS/*.PEM")
+#os.system("rm -f "+ country+"/onboarding/DCC/SCA/*.PEM")
+#os.system("rm -f "+ country+"/onboarding/DCC/UP/UP_*[0-9].PEM")
+#os.system("rm -f "+ country+"/onboarding/DCC/ISSUER/*.txt")
+os.system("ls "+repo)
 os.system("cp -a "+repo+"/onboarding/. " + country + "/" )
 
 os.system("[ -d "+country + "/onboarding/DCC/auth"+" ] && mv " + country + "/onboarding/DCC/auth "+ country+"/onboarding/DCC/TLS")
