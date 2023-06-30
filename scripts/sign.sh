a458e3a0d8f8838e1c82bb7c12b0cf55777b4ba7
Andreas Scheibal <Andreas.Scheibal@t-systems.com>
2023-06-30 13:45:03 +0000

    fix: ignore case of folders

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -57,10 +57,14 @@ declare -A USAGETOSIGNINGCFG=(
 
 declare -A DIRTOUSAGE=(
   [auth]="TLS"
+  [AUTH]="TLS"
   [TLS]="TA"
-  [csca]="TA"
+  [tls]="TA"
   [up]="TA"
-  [SCA]="TA"
+  [UP]="TA"
+  [csca]="TA"
+  [CSCA]="TA"
+  [sca]="TA"
   [SCA]="TA"
 )
 
