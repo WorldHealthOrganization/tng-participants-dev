f9a9fe9c760f7eb01526746175076f13e8917ead
SchulzeStTSI <steffen.schulze@t-systems.com>
2023-07-02 09:33:04 +0200

    test

--- scripts/review.py
+++ scripts/review.py
@@ -69,6 +69,8 @@ csrNotPresent = True
 files = glob.glob(country+"/**/Failure", recursive=True)
 comments = pr["reviews"]
 
+print(comments)
+
 if len(files): 
     approve &= False
     noFailure &= False
