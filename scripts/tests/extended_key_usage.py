dfde6e5bfe0818e5f2679a8fb97890bcace675b1
chrloch <christian.loch@gmail.com>
2023-07-07 10:15:34 +0200

    fix: no extended key usage check for UP certs

--- scripts/tests/extended_key_usage.py
+++ scripts/tests/extended_key_usage.py
@@ -6,9 +6,11 @@ from cryptography import x509
 def test_extended_key_usages(cert):
     """Extended key usage for TLS and UP certs must be set"""
     if cert.pathinfo.get('group').upper() == 'SCA'\
+    or cert.pathinfo.get('group').upper() == 'UP'\
     or cert.pathinfo.get('group').upper() == 'TLS' and cert.pathinfo.get('filename').upper().startswith('CA'):
-        #pytest.skip(reason='CA/SCA certs do not require extended key usage')
-        return # Pass: CA/SCA certs do not require extended key usage
+        #pytest.skip(reason='CA/SCA or UP certs do not require extended key usage')
+        return # Pass: CA/SCA or UP certs do not require extended key usage
+
 
     assert '2.5.29.37' in cert.extensions, 'extendedKeyUsage not in extensions'
     usages = cert.extensions['2.5.29.37'].value._usages
