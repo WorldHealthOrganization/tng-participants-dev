0c76646f12c80b728a85143d371e48d903b5ed13
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-12 09:51:51 +0200

    Update sign-json.sh

--- scripts/sign-json.sh
+++ scripts/sign-json.sh
@@ -26,6 +26,7 @@ DIRNAME=/usr/bin/dirname
 
 
 CASDIR=$1
+COUNTRY=$2
 if [[ ! -d $CASDIR ]]; then
     echo "Usage: ${BASH_SOURCE[0]} /path/to/private/key/directory"
     echo "       Missing first parameter is path to directory containing private keys"
@@ -78,6 +79,9 @@ do
     ISO3=$($BASENAME "$DIR")
     if [[ "${ISO3}" == "WHO" ]]; then continue; fi #Skip WHO keys
     echo "Processing Folder: ${ISO3}"
+
+    if [[ ! -z $COUNTRY && $COUNTRY != $DIR ]]; then continue; fi # skip countries 
+    
     for KEYDIR in $DIR/*
     do
 	if [[ ! -d $KEYDIR || -L $KEYDIR ]]; then continue; fi #not a directory 
