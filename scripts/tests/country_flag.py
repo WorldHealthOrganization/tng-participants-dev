bf327363affe5efd493ee990e96f1b8241b67e49
arajnor99 <153087310+arajnor99@users.noreply.github.com>
2024-12-04 18:56:27 +0530

    Update country_flag.py
    added Test Country XXS

--- scripts/tests/country_flag.py
+++ scripts/tests/country_flag.py
@@ -70,6 +70,8 @@ def test_country_flag(cert, pytestconfig):
                                      flag='😄', name='Test XM', numeric='8888', official_name='Test Country XXM' )
     add_country(pycountry.countries, alpha_2='XP', alpha_3='XXP', common_name='Test XP',
                                      flag='😄', name='Test XP', numeric='8889', official_name='Test Country XXP' )
+       add_country(pycountry.countries, alpha_2='XS', alpha_3='XXP', common_name='Test Xs',
+                                     flag='😄', name='Test XS', numeric='8889', official_name='Test Country XXS' )
     countries = list(pycountry.countries)
 
     if not pytestconfig.getoption('country_mode'): 
