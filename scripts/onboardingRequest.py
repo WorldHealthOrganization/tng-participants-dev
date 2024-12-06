ff6ba26a96b32f84dfd76cd9e3aef3dc1770aec5
snavthale <153069696+snavthale@users.noreply.github.com>
2024-12-06 15:44:07 +0530

    Update onboardingRequest.py

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -22,6 +22,7 @@ os.system("mkdir -p " + country)
 os.system("mkdir -p " + country+"/onboarding")
 
 allowed_domains = tuple(os.environ.get("ALLOWED_DOMAINS", "").split(','))
+print(allowed_domains)
 for domain in allowed_domains:
     source_path = os.path.join(repo, 'onboarding', domain)
     destination_path = os.path.join(country, 'onboarding', domain)
