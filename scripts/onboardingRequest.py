0825e952a12df0a4231a0275f6623f9ff14bec70
snavthale <153069696+snavthale@users.noreply.github.com>
2024-12-09 13:06:43 +0530

    Update onboardingRequest.py

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -21,7 +21,7 @@ os.system("rm -rf "+country)
 os.system("mkdir -p " + country)
 os.system("mkdir -p " + country+"/onboarding")
 
-allowed_domains = ('DCC', 'IPS-PILGRIMAGE', 'DICVP', 'PH4H')
+domains = tuple(os.getenv("ALLOWED_DOMAINS", "").split(", "))
 print(f"--- {allowed_domains} ---")
 for domain in allowed_domains:
     source_path = os.path.join(repo, 'onboarding', domain)
