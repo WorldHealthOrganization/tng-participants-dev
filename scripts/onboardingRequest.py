0e39cc80c50e1268efa802d41c2615acabedaed8
Kothari <kaushal.kothari@t-systems.com>
2026-07-30 19:04:39 +0530

    did representation of UP cert
    Signed-off-by: Kothari <kaushal.kothari@t-systems.com>

--- scripts/onboardingRequest.py
+++ scripts/onboardingRequest.py
@@ -118,6 +118,10 @@ os.system("[ -f "+country + "/onboarding/DCC/SCA/CSCA.pem"+" ] && mv " + country
 #if not os.path.exists("TT_API_ACCESS"):
       #os.system("rm -rf "+country+"/onboarding/DCC/TLS")
 
+##### Generate DID documents from the onboarded UP certificates
+os.system("echo Generating DID documents for " + country)
+os.system("python scripts/generate_did.py " + country)
+
 ##### Try to sign it
 
 if os.environ.get("ENV") != "PROD":
