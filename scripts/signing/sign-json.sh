60b9a7a1a17f2218a10108a0a4d427ab45af6854
tng-bot-dev <tng-bot-dev@who.int>
2026-09-01 14:48:51 +0530

    debug missing certs raw data
    Signed-off-by: tng-bot-dev <tng-bot-dev@who.int>

--- scripts/signing/sign-json.sh
+++ scripts/signing/sign-json.sh
@@ -4,8 +4,8 @@ set -e
 REALPATH=/bin/realpath
 BASENAME=/usr/bin/basename
 DIRNAME=/usr/bin/dirname
-SED=$(type -p sed)
-HEAD=$(type -p head)
+SED=/opt/local/bin/gsed
+HEAD=/opt/local/bin/ghead
 
 #signs keys assuming a directory structure as follows
 # $ISO3/ – three letter country code or WHO which contains key material
